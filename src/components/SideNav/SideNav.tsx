62804238458072bf93b5cc1210ab9ff4045a50a5
giuseppe-messi <giuseppe91messina@gmail.com>
2025-11-05 18:58:32 +0000

    experimenting side nav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -6,6 +6,7 @@ import {
   Pencil,
   UserSearch
   } from 'lucide-react';
+import { hoverBorder, hoverText } from '../../shared/styles';
 import { useState } from 'react';
 import { AnimatePresence, motion, type Transition } from "motion/react";
 
@@ -18,45 +19,47 @@ const items = [
   { label: "About", icon: <UserSearch size={18} /> }
 ];
 
+const transition: Transition = {
+  type: "spring",
+  stiffness: 260,
+  damping: 24
+};
+
+const sharedAnimete = (isOpen: boolean) => ({
+  initial: { opacity: 0 },
+  exit: { opacity: 1, scale: 1.1 },
+  animate: isOpen ? { opacity: 1, scale: 1.1 } : { opacity: 0, scale: 1 },
+  transition: { ...transition, delay: 0.2 }
+});
+
 export function SideNav() {
-  const [open, setOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
   const [active, setActive] = useState("Projects");
-
-  const openAnimate = open
-    ? { scale: 1.2, x: 6, y: 6 }
-    : { scale: 1, x: 0, y: 0 };
-
-  const openTransition: Transition = {
-    type: "spring",
-    stiffness: 260,
-    damping: 24
-  };
-
-  const fadeInScaleMotion = {
-    initial: { opacity: 0 },
-    exit: { opacity: 1, scale: 1.1 },
-    animate: open ? { opacity: 1, scale: 1.1 } : { opacity: 0, scale: 1 },
-    transition: { ...openTransition, delay: 0.2 }
-  };
 
   return (
     <motion.aside
-      animate={{ width: open ? NAV_OPEN : NAV_COLLAPSED }}
+      animate={{ width: isOpen ? NAV_OPEN : NAV_COLLAPSED }}
       // transition={{ type: "spring", stiffness: 100 }}
       className="sticky top-0 px-4 pt-6 bg-[var(--main-gray)]"
     >
-      <div className="flex items-center gap-6">
+      {/* <motion.div
+        layout
+        className="flex items-center gap-6"
+        transition={transition}
+      >
         <motion.img
-          animate={openAnimate}
-          transition={openTransition}
+          animate={
+            isOpen ? { scale: 1.2, x: 6, y: 6 } : { scale: 1, x: 0, y: 0 }
+          }
+          transition={transition}
           src={giuseppesAvatar}
           alt="Giuseppe Messina"
           className="w-10 h-10 rounded-full"
         />
 
-        {open && (
+        {isOpen && (
           <AnimatePresence>
-            <motion.div {...fadeInScaleMotion}>
+            <motion.div {...sharedAnimete(isOpen)}>
               <div className="whitespace-nowrap leading-tight">
                 <p className="text-sm font-semibold">Giuseppe Messina</p>
                 <p className="text-xs text-gray-500">Full Stack Developer</p>
@@ -64,21 +67,37 @@ export function SideNav() {
             </motion.div>
           </AnimatePresence>
         )}
+      </motion.div> */}
 
-        <div className="absolute right-[-10px] w-5 h-5 rounded-full bg-[var(--main-gray)] border border-[var(--text-gray)] hover:border-[var(--text-white)] transition-colors duration-300">
-          <motion.div
-            onClick={() => setOpen((o) => !o)}
-            animate={{ rotate: open ? 180 : 0 }}
-            transition={openTransition}
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer"
-          >
-            <ChevronRight
-              className="text-[var(--text-gray)] hover:text-[var(--text-white)] transition-colors duration-300"
-              size={15}
-            />
-          </motion.div>
+      <motion.div
+        layout
+        // className={clsx(
+        //   "h-10 px-2 flex items-center w-full gap-6 hoverText",
+        //   !open && "justify-center"
+        // )}
+        className={clsx("h-10 px-2 flex items-center w-full gap-6", hoverText)}
+      >
+        <div className="flex-none w-10 h-10">
+          <motion.img
+            transition={transition}
+            animate={isOpen ? { scale: 1.2 } : { scale: 1 }}
+            src={giuseppesAvatar}
+            alt="Giuseppe Messina"
+            className="w-full h-full rounded-full object-cover"
+          />
         </div>
-      </div>
+
+        {isOpen && (
+          <AnimatePresence>
+            <motion.div {...sharedAnimete(isOpen)}>
+              <div className="whitespace-nowrap leading-tight">
+                <p className="text-sm font-semibold">Giuseppe Messina</p>
+                <p className="text-xs text-gray-500">Full Stack Developer</p>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        )}
+      </motion.div>
 
       <nav className="mt-10 flex flex-col gap-3">
         {items.map((item) => (
@@ -87,29 +106,32 @@ export function SideNav() {
             whileTap={{ scale: 0.98 }}
             className={clsx(
               "w-full cursor-pointer rounded-md flex items-center",
-              active === item.label &&
-                "border border-[var(--text-gray)] hover:border-[var(--text-white)] transition-colors duration-300 bg-[#2c2c2c]"
+
+              active === item.label && `bg-[#2c2c2c] ${hoverBorder}`
             )}
-            animate={open ? { x: 6, y: 6 } : { x: 0, y: 0 }}
-            transition={openTransition}
+            animate={isOpen ? { x: 6, y: 6 } : { x: 0, y: 0 }}
+            transition={transition}
           >
             <motion.div
               layout
               // className={clsx(
-              //   "h-10 px-2 flex items-center w-full gap-6 text-[var(--text-gray)] hover:text-[var(--text-white)] transition-colors duration-300",
+              //   "h-10 px-2 flex items-center w-full gap-6 hoverText",
               //   !open && "justify-center"
               // )}
-              className="h-10 px-2 flex items-center w-full gap-6 text-[var(--text-gray)] hover:text-[var(--text-white)] transition-colors duration-300"
+              className={clsx(
+                "h-10 px-2 flex items-center w-full gap-6",
+                hoverText
+              )}
             >
               <motion.div
-                transition={openTransition}
-                animate={open ? { scale: 1.2 } : { scale: 1 }}
+                transition={transition}
+                animate={isOpen ? { scale: 1.2 } : { scale: 1 }}
               >
                 {item.icon}
               </motion.div>
 
-              {open && (
-                <motion.span {...fadeInScaleMotion} className="text-sm">
+              {isOpen && (
+                <motion.span {...sharedAnimete(isOpen)} className="text-sm">
                   {item.label}
                 </motion.span>
               )}
@@ -117,6 +139,28 @@ export function SideNav() {
           </motion.button>
         ))}
       </nav>
+
+      <div
+        className={clsx(
+          "absolute top-[38px] right-[-10px] w-5 h-5 rounded-full bg-[var(--main-gray)]",
+          hoverBorder
+        )}
+      >
+        <motion.div
+          onClick={() => setIsOpen((o) => !o)}
+          animate={{ rotate: isOpen ? 180 : 0 }}
+          transition={transition}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+        >
+          <ChevronRight
+            className={clsx(
+              "absolute left-1/2 top-1/2 -translate-x-[47%] -translate-y-1/2",
+              hoverText
+            )}
+            size={15}
+          />
+        </motion.div>
+      </div>
     </motion.aside>
   );
 }
---
+++ src/shared/styles.ts
@@ -0,0 +1,14 @@
+export const hoverBorder = `
+  group
+  border border-[var(--text-gray)]
+  hover:border-[var(--text-white)]
+  group-hover:border-[var(--text-white)]
+  transition-colors duration-300
+`;
+
+export const hoverText = `
+  text-[var(--text-gray)]
+  hover:text-[var(--text-white)]
+  group-hover:text-[var(--text-white)]
+  transition-colors duration-300
+`;
